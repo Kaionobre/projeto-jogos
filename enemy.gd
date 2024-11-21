a843542ed6f12e9567a4c3e9fc25fa3658930cf8
Kaio Nóbrega <kaionobrega00@gmail.com>
2024-11-21 09:53:48 -0300

    Update enemy.gd

--- enemy.gd
+++ enemy.gd
@@ -1,8 +1,6 @@
 extends RigidBody2D
 
 var speed = 0.0
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	var fly_speed = 130.0
@@ -10,9 +8,7 @@ func _ready():
 	var walk_speed = 130.0
 
 	
-	# Obtem um array com os nomes das animações ["walk", "swim", "fly"]
 	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
-   	# Inicia a animação definida aleatoriamente entre os índices 0 e 2 
 	var random_animation_index = randi() % mob_types.size()
 	var random_animation = mob_types[random_animation_index]
 	
